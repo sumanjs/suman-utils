587a8995283aa755c56b50e801503df353d2278a
Olegzandr <alex@oresoftware.com>
2017-09-23 16:25:13 -0700

    pdev:-f

--- test/target/one.test.js
+++
@@ -1,8 +0,0 @@
-var utils = require('suman-utils');
-[
-    utils.isGeneratorFn(function* () { }),
-    utils.isGeneratorFn2(function* () { }),
-    utils.isGeneratorFn2(function () { })
-].forEach(function (item) {
-    console.log('isGen => ', item);
-});
--- test/target/test1.js
+++
@@ -1,3 +0,0 @@
-var sumanUtils = require('suman-utils');
-var arr = sumanUtils.getArrayOfDirsToBuild('/Users/Olegzandr/WebstormProjects/oresoftware/suman/test-target', '/Users/Olegzandr/WebstormProjects/oresoftware/suman/test/integration-tests/test0.js');
-console.log(arr);
