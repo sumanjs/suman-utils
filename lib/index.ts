506a09ad7bc6096d4dd0a3e6b2b7b6c22b0b1c01
Olegzandr VD <alex@oresoftware.com>
2017-12-04 19:54:36 -0800

    pdev:set

--- lib/index.ts
+++ lib/index.ts
@@ -15,9 +15,10 @@ process.on('warning', function (w: any) {
 //core
 import * as fs from 'fs';
 import * as path from 'path';
-import * as util from 'util';
-import * as assert from 'assert';
-import * as events from 'events';
+import util = require('util');
+import assert = require('assert');
+import events = require('events');
+import qs = require('querystring');
 
 //npm
 import async = require('async');
@@ -71,7 +72,7 @@ export const vlt = function (val: number): boolean {
 };
 
 export const checkStatsIsFile = function (item: string) {
-
+  
   try {
     return fs.statSync(item).isFile();
   }
@@ -84,7 +85,7 @@ export const checkStatsIsFile = function (item: string) {
 };
 
 export const mapToTargetDir = function (item: string): MapToTargetDirResult {
-
+  
   const projectRoot = process.env.SUMAN_PROJECT_ROOT;
   // note => these values were originally assigned in suman/index.js,
   // were then passed to suman server, which then required this file
@@ -92,25 +93,25 @@ export const mapToTargetDir = function (item: string): MapToTargetDirResult {
   const testSrcDir = process.env.TEST_SRC_DIR;
   const testTargetDir = process.env.TEST_TARGET_DIR;
   const testTargetDirLength = String(testTargetDir).split(path.sep).length;
-
+  
   item = path.resolve(path.isAbsolute(item) ? item : (projectRoot + '/' + item));
-
+  
   let itemSplit = String(item).split(path.sep);
   itemSplit = itemSplit.filter(i => i); // get rid of pesky ['', first element
-
+  
   const originalLength = itemSplit.length;
   const paths = removeSharedRootPath([projectRoot, item]);
   const temp = paths[1][1];
-
+  
   let splitted = temp.split(path.sep);
   splitted = splitted.filter(i => i); // get rid of pesky ['', first element
-
+  
   while ((splitted.length + testTargetDirLength) > originalLength + 1) {
     splitted.shift();
   }
-
+  
   const joined = splitted.join(path.sep);
-
+  
   return {
     originalPath: item,
     targetPath: path.resolve(testTargetDir + '/' + joined)
@@ -118,16 +119,16 @@ export const mapToTargetDir = function (item: string): MapToTargetDirResult {
 };
 
 export const findApplicablePathsGivenTransform = function (sumanConfig: Object, transformPath: string, cb: Function) {
-
+  
   const dir = path.dirname(transformPath);
   const results: Array<string> = [];
-
+  
   let firstPass = true;
-
+  
   (function searchDir(dir: string, cb: Function) {
-
+    
     fs.readdir(dir, function (err, items) {
-
+      
       if (firstPass === false) {
         for (let i = 0; i < items.length; i++) {
           if (String(items[i]).match(/@transform.sh/)) {
@@ -135,48 +136,48 @@ export const findApplicablePathsGivenTransform = function (sumanConfig: Object, 
           }
         }
       }
-
+      
       // after this, we are in subdirectories
       firstPass = false;
-
+      
       async.eachLimit(items, 3, function (item: string, cb: Function) {
-
+        
         const fullPath = path.resolve(dir + '/' + item);
-
+        
         fs.stat(fullPath, function (err, stats) {
-
+          
           if (err) {
             log.error(err.stack);
             return cb();
           }
-
+          
           if (stats.isFile()) {
             if (String(fullPath).match(/\/@src\//)) {
               results.push(fullPath);
             }
             return cb();
           }
-
+          
           if (stats.isDirectory()) {
             if (String(fullPath).match(/\/node_modules\//)) {
               return cb();
             }
-
+            
             searchDir(fullPath, cb);
-
+            
           }
-
+          
         });
-
+        
       }, cb);
     });
-
+    
   })(dir, function (err: Error) {
-
+    
     cb(err, results);
-
+    
   });
-
+  
 };
 
 export const isSumanSingleProcess = function (): boolean {
@@ -203,16 +204,16 @@ export const buildDirsWithMkDirp = function (paths: Array<string>, cb: Function)
 };
 
 export const getArrayOfDirsToBuild = function (testTargetPath: string, p: string): string | undefined {
-
+  
   // => p is expected to be a path to a file, not a directory
   let temp: string;
   const l = path.normalize('/' + testTargetPath).split('/').length;
   const items = path.normalize('/' + p).split('/');
-
+  
   if (fs.statSync(p).isFile()) {
     items.pop(); // always get rid of the first file
   }
-
+  
   if (items.length >= l) {
     temp = path.normalize(items.slice(l).join('/'));
   }
@@ -223,7 +224,7 @@ export const getArrayOfDirsToBuild = function (testTargetPath: string, p: string
     log.error('warning: testTargetDir =>', testTargetPath);
     console.log('\n');
   }
-
+  
   if (temp) {
     return path.resolve(testTargetPath + '/' + temp);
   }
@@ -231,15 +232,15 @@ export const getArrayOfDirsToBuild = function (testTargetPath: string, p: string
     //explicit for your pleasure; and so TS does not complain
     return undefined;
   }
-
+  
 };
 
 export const checkIfPathAlreadyExistsInList = function (paths: Array<string>, p: string, index: number): boolean {
-
+  
   // assume paths =>  [/a/b/c/d/e]
   // p => /a/b/c
   // this fn should return true then
-
+  
   return paths.some(function (pth, i) {
     if (i === index) {
       // we ignore the matching item
@@ -247,17 +248,17 @@ export const checkIfPathAlreadyExistsInList = function (paths: Array<string>, p:
     }
     return String(pth).indexOf(p) === 0;
   });
-
+  
 };
 
 export const buildDirs = function (dirs: Array<string>, cb: Function): void {
-
+  
   if (dirs.length < 1) {
     return process.nextTick(cb);
   }
-
+  
   async.eachSeries(dirs, function (item: string, cb: Function): void {
-
+    
     fs.mkdir(item, function (err: Error) {
       if (err && !String(err.stack).match(/eexist/i)) {
         log.error(err.stack || err);
@@ -267,9 +268,9 @@ export const buildDirs = function (dirs: Array<string>, cb: Function): void {
         cb(null);
       }
     });
-
+    
   }, cb);
-
+  
 };
 
 export const padWithFourSpaces = function (): string {
@@ -281,38 +282,38 @@ export const padWithXSpaces = function (x: number): string {
 };
 
 export const removePath = function (p1: string, p2: string): string {
-
+  
   assert(path.isAbsolute(p1) && path.isAbsolute(p2), 'Please pass in absolute paths, ' +
     'p1 => ' + util.inspect(p1) + ', p2 => ' + util.inspect(p2));
-
+  
   const split1 = String(p1).split(path.sep);
   const split2 = String(p2).split(path.sep);
-
+  
   const newPath: Array<string> = [];
-
+  
   const max = Math.max(split1.length, split2.length);
-
+  
   for (let i = 0; i < max; i++) {
     if (split1[i] !== split2[i]) {
       newPath.push(split1[i]);
     }
   }
-
+  
   return newPath.join(path.sep);
-
+  
 };
 
 export const findSharedPath = function (p1: string, p2: string): string {
-
+  
   const split1 = String(p1).split(path.sep);
   const split2 = String(p2).split(path.sep);
-
+  
   //remove weird empty strings ''
   const one = split1.filter(i => i);
   const two = split2.filter(i => i);
-
+  
   const max = Math.max(one.length, two.length);
-
+  
   // if (split1[0] === '') {
   //     split1.shift();
   // }
@@ -320,10 +321,10 @@ export const findSharedPath = function (p1: string, p2: string): string {
   // if (split2[0] === '') {
   //     split2.shift();
   // }
-
+  
   let i = 0;
   let shared: Array<string> = [];
-
+  
   while (one[i] === two[i] && i < max) {
     shared.push(one[i]);
     i++;
@@ -332,7 +333,7 @@ export const findSharedPath = function (p1: string, p2: string): string {
         'second array => ' + two);
     }
   }
-
+  
   shared = shared.filter(i => i);
   return path.resolve(path.sep + shared.join(path.sep));
 };
@@ -343,25 +344,25 @@ export const removeProjectRootFromPath = function (p: string) {
 };
 
 export const removeSharedRootPath = function (paths: Array<string>): Array<Array<string>> {
-
+  
   if (paths.length < 2) {   //  paths = ['just/a/single/path/so/letsreturnit']
     return paths.map(function (p) {
       return [p, path.basename(p), removeProjectRootFromPath(p)];
     });
   }
-
+  
   let shared: string | Array<string>;
-
+  
   paths.forEach(function (p) {
-
+    
     //assume paths are absolute before being passed here
     p = path.normalize(p);
-
+    
     if (shared) {
       const arr = String(p).split('');
-
+      
       let i = 0;
-
+      
       arr.every(function (item, index) {
         if (String(item) !== String(shared[index])) {
           i = index;
@@ -369,16 +370,16 @@ export const removeSharedRootPath = function (paths: Array<string>): Array<Array
         }
         return true;
       });
-
+      
       shared = shared.slice(0, i);
-
+      
     }
     else {
       shared = String(p).split('');
     }
-
+    
   });
-
+  
   return paths.map(function (p) {
     const basenameLngth = path.basename(p).length;
     return [
@@ -387,7 +388,7 @@ export const removeSharedRootPath = function (paths: Array<string>): Array<Array
       removeProjectRootFromPath(p)
     ];
   });
-
+  
 };
 
 export const checkForValInStr = function (str: string, regex: RegExp, count: number): boolean {
@@ -403,14 +404,14 @@ export const isGeneratorFn2 = function (fn: Function): boolean {
 };
 
 export const isGeneratorFn = function (fn: Function): boolean {
-
+  
   if (typeof fn !== 'function') {
     return false;
   }
   let fnStr = toStr.call(fn);
   return ((fnStr === '[object Function]' || fnStr === '[object GeneratorFunction]') && isFnRegex.test(fnToStr.call(fn))
     || (fn.constructor.name === 'GeneratorFunction' || fn.constructor.displayName === 'GeneratorFunction'));
-
+  
 };
 
 export const isArrowFunction = function (fn: Function): boolean {
@@ -444,7 +445,7 @@ export const findProjectRoot = function (p: string): string {
 export const findProjRoot = findProjectRoot;
 
 export const once = function (ctx: Object, fn: Function): Function {
-
+  
   let callable = true;
   return function callOnce(err: Error) {
     if (callable) {
@@ -452,16 +453,33 @@ export const once = function (ctx: Object, fn: Function): Function {
       return fn.apply(ctx, arguments);
     }
     else {
-      _suman.logWarning('suman implementation warning => function was called more than once => ' + fn ? fn.toString() : '');
-      if (err) {
-        _suman.logError('warning => ', err.stack || util.inspect(err));
-      }
+      _suman.logWarning('Suman implementation warning => function was called more than once => ' + fn ? fn.toString() : '');
+      err && _suman.logError('warning:', err.stack || util.inspect(err));
     }
   }
 };
 
-export const onceTO = function (ctx: Object, fn: Function, to: Timer): Function {
+export const onceWithCache = function (fn: Function) {
+  let cache: any = null;
+  
+  return function (cb: Function) {
+    
+    if (cache) {
+      process.nextTick(cb, null, cache);
+      return;
+    }
+    
+    fn.call(null, function (err: Error, val: any) {
+      if (!err) {
+        cache = val || {'Suman says': 'This is a dummy-cache val. See => sumanjs.org/tricks-and-tips.html'};
+      }
+      cb.call(null, err, cache);
+    });
+    
+  }
+};
 
+export const onceTO = function (ctx: Object, fn: Function, to: Timer): Function {
   let callable = true;
   return function callOnce(err: Error) {
     if (callable) {
@@ -517,26 +535,26 @@ export const repeatCharXTimes = function (char: string, num: number) {
 };
 
 export const createCleanStack = function (str: String, $ignore?: Array<string | RegExp>) {
-
+  
   const ignore = ($ignore || [/node_modules/, /next_tick.js/, /sumanjs/]).map(function (r) {
     return r instanceof RegExp ? r : new RegExp(r);
   });
-
+  
   return String(str).split('\n').filter(function (s) {
-
+    
     if (/\/sumanjs\/test\//.test(s)) {
       return true;
     }
-
+    
     return !ignore.some(function (ig) {
       return ig.test(s);
     })
   });
-
+  
 };
 
 export const onceAsync = function (ctx: Object, fn: Function): Function {
-
+  
   let callable = true;
   return function callOnce(err: Error) {
     const args = Array.from(arguments);
@@ -552,7 +570,7 @@ export const onceAsync = function (ctx: Object, fn: Function): Function {
         log.error('warning: ', err.stack || util.inspect(err));
       }
     }
-
+    
   }
 };
 
@@ -572,7 +590,7 @@ export const customStringify = function (v: any) {
 };
 
 export const makePathExecutable = function (runPath: string, cb: Function) {
-
+  
   if (runPath) {
     fs.chmod(runPath, 0o777, cb);
   }
@@ -581,21 +599,50 @@ export const makePathExecutable = function (runPath: string, cb: Function) {
   }
 };
 
-export const checkForEquality = function (arr1: Array<string>, arr2: Array<string>): boolean {
+export const getEnvObjFromStr2 = function (str: string): Object {
+  
+  const ret = {} as any;
+  
+  String(str).split(/\s+/g).forEach(function (s) {
+    const kv = String(s).split('=');
+    if (kv.length > 2) {
+      throw new Error('String representing env variables is malformed => ' + str);
+    }
+    
+    const key = String(kv[0]).trim();
+    const val = String(kv[1]).trim();
+    
+    if (key in ret) {
+      throw new Error('Environment variable was set more than once => ' + str);
+    }
+    
+    ret[key] = val;
+    
+  });
+  
+  return ret;
+  
+};
 
+export const getEnvObjFromStr = function (str: string): Object {
+  return qs.parse(String(str), ' ', '=');
+};
+
+export const checkForEquality = function (arr1: Array<string>, arr2: Array<string>): boolean {
+  
   if (arr1.length !== arr2.length) {
     return false;
   }
-
+  
   arr1 = arr1.sort();
   arr2 = arr2.sort();
-
+  
   for (let i = 0; i < arr1.length; i++) {
     if (String(arr1[i]) !== String(arr2[i])) {
       return false;
     }
   }
-
+  
   return true;
 };
 
@@ -610,7 +657,7 @@ export const isStringWithPositiveLn = function (s: string) {
 };
 
 export const findNearestRunAndTransform = function (root: string, pth: string, cb: Function) {
-
+  
   try {
     if (!fs.statSync(pth).isDirectory()) {
       pth = path.dirname(pth);
@@ -619,18 +666,18 @@ export const findNearestRunAndTransform = function (root: string, pth: string, c
   catch (err) {
     return process.nextTick(cb, err);
   }
-
+  
   let results: Array<any> = [];
   let upPath: string = pth;
-
+  
   async.whilst(function () {
-
+    
     return upPath.length >= root.length;
-
+    
   }, function (cb: Function) {
-
+    
     async.parallel({
-
+      
       run: function (cb: Function) {
         let p = path.resolve(upPath + '/@run.sh');
         fs.stat(p, function (err, stats) {
@@ -640,7 +687,7 @@ export const findNearestRunAndTransform = function (root: string, pth: string, c
           cb();
         });
       },
-
+      
       transform: function (cb: Function) {
         let p = path.resolve(upPath + '/@transform.sh');
         fs.stat(p, function (err, stats) {
@@ -650,7 +697,7 @@ export const findNearestRunAndTransform = function (root: string, pth: string, c
           cb();
         });
       },
-
+      
       config: function (cb: Function) {
         let p = path.resolve(upPath + '/@config.json');
         fs.stat(p, function (err, stats) {
@@ -660,33 +707,33 @@ export const findNearestRunAndTransform = function (root: string, pth: string, c
           cb();
         });
       }
-
+      
     }, function (err: Error) {
       upPath = path.resolve(upPath + '/../');
       cb(err);
     });
-
+    
   }, function (err: Error) {
     if (err) {
       return cb(err);
     }
-
+    
     let ret: INearestRunAndTransformRet = results.reduce(function (prev, curr) {
       return (curr ? Object.assign(prev, curr) : prev);
     }, {});
-
+    
     cb(null, ret);
-
+    
   });
-
+  
 };
 
 export const findSumanMarkers = function (types: Array<string>, root: string, files: Array<string>, cb: IMapCallback): void {
-
+  
   //TODO: we can stop when we get to the end of all the files in files array
   const sumanHelpersDirRegex = new RegExp(_suman.sumanHelperDirRoot);
   const map: any = {};
-
+  
   let addItem = function (item: string): void {
     let filename = path.basename(item);
     types.forEach(function (t) {
@@ -698,39 +745,39 @@ export const findSumanMarkers = function (types: Array<string>, root: string, fi
       }
     });
   };
-
+  
   (function getMarkers(dir: string, cb: Function) {
-
+    
     if (sumanHelpersDirRegex.test(dir)) {
       return process.nextTick(cb);
     }
-
+    
     fs.readdir(dir, function (err, items) {
-
+      
       if (err) {
         log.warning('warning: fs error while creating suman markers map.');
         log.warning('warning: ' + err.message);
         return cb(null);
       }
-
+      
       items = items.map(function (item) {
         return path.resolve(dir, item);
       });
-
+      
       async.eachLimit(items, 5, function (item: string, cb: Function) {
-
+        
         if (sumanHelpersDirRegex.test(item)) {
           return process.nextTick(cb);
         }
-
+        
         fs.stat(item, function (err, stats) {
-
+          
           if (err) {
             log.warning('warning: fs error while creating suman markers map.');
             log.warning('warning:' + err.message);
             return cb(null);
           }
-
+          
           if (stats.isFile()) {
             addItem(item);
             cb(null);
@@ -752,17 +799,17 @@ export const findSumanMarkers = function (types: Array<string>, root: string, fi
             log.warning(`warning: possibly a symlink (symlinks not yet supported) => "${item}".`);
             cb(null);
           }
-
+          
         });
-
+        
       }, cb);
-
+      
     });
-
+    
   })(root, function (err: Error) {
     err ? cb(err) : cb(null, map);
   });
-
+  
 };
 
 export const isObject = function (v: any) {
